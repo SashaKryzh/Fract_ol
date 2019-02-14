dc7b66bc1078ffe2e318ce3da574839af19bb32e
okryzhan <sashakryzh@gmail.com>
2019-02-14 09:08:23 +0200

    some

--- main.c
+++ main.c
@@ -31,9 +31,70 @@ void	init_win(t_fract *fract)
 		&fract->bpp, &fract->size_line, &fract->endian);
 }
 
+void	pixel_fill(t_fract *fract, int color, int x, int y)
+{
+	mlx_pixel_put(fract->mlx_ptr, fract->win_ptr, x, y, color);
+}
+
 void	Mandelbrot_set(t_fract *fract)
 {
-	int		i;
+	double xmin = -2.0;
+	double xmax = 1.5;
+	double ymin = -2.0;
+	double ymax = 2.0;
+
+	int xres = 1500; // Set size
+	int	yres = (xres * (ymax - ymin) / (xmax - xmin)); // Set size
+
+	double	dx = (xmax - xmin) / xres; // Pixel width
+	double	dy = (ymax - ymin) / yres; // Pixel height
+
+	double	x; // Curent
+	double	y; // Curent
+
+	double	u; // Iterated
+	double	v; // Iterated
+	double	u2;
+	double	v2;
+
+	int i; // Counter
+	int j; // Counter
+
+	int	k; // Iterations
+
+	j = 0;
+	while (j < yres)
+	{
+		y = ymax - j * dy;
+		i = 0;
+		while (i < xres)
+		{
+			x = xmin + i * dx;
+			u = 0.0;
+			v = 0.0;
+			u2 = pow(u, 2);
+			v2 = pow(v, 2);
+			k = 0;
+			while (k < 65555 && u2 + v2 < 4.0)
+			{
+				v = 2 * u * v + y;
+				u = u2 - v2 + x;
+				u2 = pow(u, 2);
+				v2 = pow(v, 2);
+				k++;
+			}
+			i++;
+			if (k > 65555)
+			{
+				pixel_fill(fract, 0, i, j);
+			}
+			else
+			{
+				pixel_fill(fract, 2147483647, i, j);
+			}
+		}
+		j++;
+	}
 }
 
 void	magic(t_fract *fract)
